07d91060ae018b3763e52658ac4587585b09d822
nighostchris <chrisliupascal@gmail.com>
2019-07-25 10:18:49 +0800

    Reconstruct how main.js import vuetify and router

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,44 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import Router from 'vue-router';
+
 import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
+import Home from './views/Home.vue';
+
+Vue.use(Vuetify);
+Vue.use(Router);
 
 Vue.config.productionTip = false;
 
+const vuetify = new Vuetify();
+
+const route = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/index.html',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+  ],
+});
+
 new Vue({
-  router,
   vuetify,
+  route,
   render: h => h(App),
 }).$mount('#app');
