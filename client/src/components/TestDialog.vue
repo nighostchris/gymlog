3cbf3f309dcdf478cc599728e9944e1b6df8dc99
nighostchris <chrisliupascal@gmail.com>
2019-07-26 15:06:56 +0800

    Designing prototype for choosing gym posture by dialog

---
+++ client/src/components/TestDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog v-model="dialog" xs7 md6 lg6 xl6>
+    <template v-slot:activator="{ on }">
+      <v-btn color="red" dark v-on="on">
+        Choose
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Select the posture</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-layout>
+            <v-flex xs12 sm6>
+              <v-select
+                :items="['0-17', '18-29', '30-54', '54+']"
+                label="Age*"
+                required
+              ></v-select>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-autocomplete
+                :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey']"
+                label="Interests"
+                multiple
+              ></v-autocomplete>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+        <v-btn color="blue darken-1" text @click="dialog = false">Proceed</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import RegisterBox from './components/RegisterBox.vue';
+import TestDialog from './components/TestDialog.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/test',
       name: 'test',
       component: RegisterBox,
+    },
+    {
+      path: '/dialog',
+      name: 'dialog',
+      component: TestDialog,
     },
     {
       path: '/about',
