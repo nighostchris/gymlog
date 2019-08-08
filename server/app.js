b2bbe00c9cb00aa2f118926316d5137b46996abb
nighostchris <chrisliupascal@gmail.com>
2019-08-08 11:20:58 +0800

    addRecord and getRecord function without testing

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const loginFunctions = require('./database/functions/login');
 const tokenFunctions = require('./database/functions/token');
+const recordFunctions = require('./database/functions/record');
 const registerFunctions = require('./database/functions/register');
 
 const app = express();
@@ -14,6 +15,9 @@ app.route('/api/v1/users')
 app.route('/api/v1/token')
 	.get(loginFunctions.login)
 	.post(tokenFunctions.validateToken);
+
+app.route('/api/v1/record')
+	.post(recordFunctions.addRecord);
 
 app.listen(3000, () => {
 	console.log('API available on port 3000');
---
+++ server/database/functions/record.js
@@ -0,0 +1,93 @@
+const jwt = require('jsonwebtoken');
+const secretJson = require('../secret.json');
+const models = require('../models');
+
+const Users = models.users;
+const Records = models.records;
+const Routines = models.routine;
+
+const validateToken = (req, res) => {
+  try {
+    const result = jwt.verify(req.body.token, secretJson.secret);
+    return result.verify.sub;
+  } catch (e) {
+    res.status(400);
+    res.json({ err: e.message });
+  }
+};
+
+exports.addRecord = (req, res) => {
+  const uName = validateToken(req, res);
+
+	Users.findOne({
+		where: {
+			username: uName,
+		},
+	}).then((data) => {
+		if (!data) {
+			res.status(400);
+			res.json({ err: 'Non-existing user' });
+		}
+
+    const record = {
+      user_id: data.id,
+      date: req.body.date,
+      exercise: JSON.parse(req.body.exercise),
+      sets: JSON.parse(req.body.sets),
+    };
+
+    const routine = {
+      user_id: data.id,
+      name: req.body.name,
+      remark: req.body.remark,
+      exercise: JSON.parse(req.body.exercise),
+    };
+
+    Routines.create(routine).then(() => {
+      Records.create(record).then((success) => {
+        res.status(200);
+        res.json({ "success": "Exercises recorded" });
+      }).catch((e) => {
+        res.status(500);
+        res.send(e);
+      });
+    }).catch((e) => {
+      res.status(500);
+      res.send(e);
+    });
+    
+	}).catch((e) => {
+		res.status(500);
+		res.json({ err: e });
+	});
+};
+
+exports.getRecord = (req, res) => {
+  const uName = validateToken(req, res);
+
+	Users.findOne({
+		where: {
+			username: uName,
+		},
+	}).then((data) => {
+		if (!data) {
+			res.status(400);
+			res.json({ err: 'Non-existing user' });
+    }
+
+    Records.findAll({
+      where: {
+        date: req.body.date,
+      },
+    }).then((data) => {
+      res.status(200);
+      res.json(data);
+    }).catch((e) => {
+      res.status(500);
+      res.json({ err: e });
+    });
+  }).catch((e) => {
+		res.status(500);
+		res.json({ err: e });
+	});
+}
